775fb196f3a799bb54dd28fbcf386c3ad641b338
Agboola Samson <agboolasamson602@gmail.com>
2025-03-21 21:14:53 +0100

    removed the cv file and redirect to google docs instead

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Contact from "./pages/Contact";
 import Footer from './components/Footer';
-import Download from './components/Download';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <div className="App">
         <Nav />
-        <Download />
+
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='projects' element={<Projects />} />
