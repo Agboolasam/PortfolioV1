8d57fa43b75958f8c33e184816176c393aa8b99e
Agboola Samson <agboolasamson602@gmail.com>
2025-03-21 01:04:40 +0100

    added picture

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
-
+import img from '../assets/img.png';
 export default function Home() {
     return (
         <main>
-            <h1>Home
-
-            </h1>
+            <section id="left"></section>
+            <section id="right">
+                <img src={img} alt="Agboola samson ifedolapo's portrait" />
+            </section>
 
         </main>
     )
