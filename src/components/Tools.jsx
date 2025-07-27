6d6201240c5bbba41a549e0b69e0396498d3be07
Agboola Samson <agboolasamson602@gmail.com>
2025-07-27 20:35:01 +0100

    Animation on the tools

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -1,11 +1,19 @@
 /* eslint-disable react/prop-types */
 
+
+
 function Tools({ tool, icon, description }) {
     return (
         <div className="tools">
-            <img src={icon} alt={`${tool} icon`} />
-            <h3>{tool}</h3>
-            <p>{description}</p>
+            <div className="tools-icon">
+                <img src={icon} alt={`${tool} icon`} />
+
+            </div>
+            <div className="tools-text">
+                <h3>{tool}</h3>
+                <p>{description}</p>
+            </div>
+
         </div>
     )
 }
