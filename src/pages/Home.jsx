816beb9975388186330914658d42a3c0170f3d39
Agboola Samson <agboolasamson602@gmail.com>
2025-07-27 17:10:12 +0100

    rearranged the intro section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,23 +6,27 @@ export default function Home() {
 
     return (
         <main>
-            <section id="left">
-                <p id='name'>Hi, I'm Agboola Samson Ifedolapo</p>
-                <h2 id='changingtext'><span id='iam'>I am a</span> <TypingEffect /></h2>
+            <section id='intro'>
+                <EntryAnimate>
+                    <section id="right">
+                        <img src={img} loading='lazy' alt="Agboola samson ifedolapo's portrait" />
+                    </section>
 
-                <EntryAnimate threshold={0.2}>
-                    <p id='about'>I'm a passionate and dedicated software engineer with a strong foundation in both frontend and backend technologies. My expertise lies in creating seamless user experiences and building robust backend systems. I thrive on challenges and continuously seek opportunities to learn and grow in the ever-evolving world of technology.<br /><br />With a background in mechanical design engineering, I bring a unique perspective to software development, combining technical skills with a creative approach to problem-solving. I am committed to delivering high-quality solutions that meet the needs of users and drive business success.</p>
                 </EntryAnimate>
-            </section>
 
-            <EntryAnimate>
-                <section id="right">
-                    <img src={img} alt="Agboola samson ifedolapo's portrait" />
+                <section id="left">
+                    <p id='name'>Hi, I'm Agboola Samson Ifedolapo</p>
+                    <h2 id='changingtext'><span id='iam'>I am a</span> <TypingEffect /></h2>
+
+                    <EntryAnimate threshold={0.2}>
+                        <p id='about'>I'm a passionate and dedicated software engineer with a strong foundation in both frontend and backend technologies. My expertise lies in creating seamless user experiences and building robust backend systems. I thrive on challenges and continuously seek opportunities to learn and grow in the ever-evolving world of technology.<br /><br />With a background in mechanical design engineering, I bring a unique perspective to software development, combining technical skills with a creative approach to problem-solving. I am committed to delivering high-quality solutions that meet the needs of users and drive business success.</p>
+                    </EntryAnimate>
                 </section>
 
-            </EntryAnimate>
 
 
+
+            </section>
         </main >
     )
 }
